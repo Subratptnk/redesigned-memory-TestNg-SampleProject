7ec3e0bb24de1f1039ad5166d88907140ac92d91
Subs <subratpattanaik20@gmail.com>
2022-05-07 11:24:10 +0530

    First TestNg framework Updates

---
+++ src/test/TestNgBasisc.java
@@ -0,0 +1,40 @@
+package test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNgBasisc {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "D:/Subrat2022/SELENIUM/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.freecrm.com/");
+	}
+	
+	@Test
+	public void verifyPageTitleTest() {
+		String ExpectedTitle = "Free CRM software for customer relationship management, sales, marketing campaigns and support.";
+		String pageTitile =  driver.getTitle();
+		//System.out.println("The page tittle is "+pageTitile);		
+		Assert.assertEquals(pageTitile, ExpectedTitle);
+	}
+	
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
